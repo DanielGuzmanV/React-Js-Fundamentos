5d065c6f5bf74668bc7995ab75ac92e4433614b4
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-02 10:53:32 -0400

    creamos el primer componente mas su estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import './App.css'
+import Menu from "./components/Menu"
+
 
 function App() {
   return (
     <div>
       <h1>Inicio del proyecto</h1>
+      <Menu/>
+      <h2>Etiqueta Jsx</h2>
     </div>
 
   )
