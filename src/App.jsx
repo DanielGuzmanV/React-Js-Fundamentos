80813f7f8458c31d3278fc5f3fea91fe69e15d4a
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-07 12:17:14 -0400

    se configuro las rutas en el main y app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,3 @@
-import { useState } from "react";
-import AtributoReactivo from "./components/AtributoReactivo";
-import Renderizado from "./components/Renderizado";
-import Listas from "./components/Listas";
-import PropsPadrehijo from "./components/PropsPadrehijo";
-import ClassComponent from "./components/ClassComponent";
-import PropsHijopadre from "./components/PropsHijopadre";
-import PageStateUp from "./pages/PageStateUp";
-import PageUseEffect from "./pages/PageUseEffect";
-import PageMenu from "./pages/PageMenu";
-import PagePropsPadrehijo from "./pages/PagePropsPadrehijo";
-import PagePropsHijoPadre from "./pages/PagePropsHijoPadre";
-import PageFetchapi from "./pages/PageFetchapi";
-import PageFormularios from "./pages/PageFormularios";
-
-
 function App() {
 
   // Evento Independiente y reutilizable:
@@ -25,40 +9,9 @@ function App() {
     <div>
       <h1 onClick={greetConsole} >Inicio del proyecto</h1>
 
-      <PageMenu/>
-
-      <hr/>
-      <AtributoReactivo/>
-
-      <hr/>
-      <Renderizado/>
-
-      <hr/>
-      <Listas/>
-
-      <hr/>
-      <PagePropsPadrehijo/>
-
-      <hr/>
-      <ClassComponent/>
-
-      <hr/>
-      <PagePropsHijoPadre/>
-
-      <hr/>
-      <PageStateUp/>
-
-      <hr/>
-      <PageUseEffect/>
-
-      <hr/>
-      <PageFetchapi/>
-
+      <MyRoutes/>
       <hr/>
 
-      <PageFormularios/>
-
-      
     </div>
   )
 }
