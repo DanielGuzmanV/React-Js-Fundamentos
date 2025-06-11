9f34519c14a4788ab0498c7a452b77e7d8a6a2d2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-11 12:07:53 -0400

    se configuro el app para ir al main principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import MyRoutes from "./routes/Routes"
 import reactLogo from "./assets/react.svg"
+import { useNavigate } from "react-router-dom";
 
 function App() {
+  const mainNavigation = useNavigate(); 
 
   // Evento Independiente y reutilizable:
   const greetConsole = () => {
     console.log('Saludo desde la consola');
+    mainNavigation('/')
   }
 
   return (
