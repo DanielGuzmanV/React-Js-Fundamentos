05f9c6f5d82efbe3244be81f78e6f70c82c83238
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-11 10:35:40 -0400

    se configuro el context en el main

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { UserProviderWrapper } from './context/user_context.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <UserProviderWrapper>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProviderWrapper>
   </StrictMode>,
 )
