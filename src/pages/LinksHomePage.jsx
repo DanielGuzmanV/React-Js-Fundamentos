b34c3b8670eda37cf871a40c74e5c162929db762
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-11 08:56:13 -0400

    Gestion de estado de error agregado a LinksHomePage

--- src/pages/LinksHomePage.jsx
+++ src/pages/LinksHomePage.jsx
@@ -4,13 +4,18 @@ import PostCard from "../components/PostCard";
 
 function LinksHomePage() {
   const [posts, setPosts] = useState([]);
+  const [valueError, setError] = useState(false);
 
   const getPosts = async() => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    console.log(data);
-
-    setPosts(data);
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data = await response.json();
+      console.log(data);
+      setPosts(data);
+      
+    } catch (error) {
+      setError(true);
+    }
   }
 
   useEffect(() => {
@@ -25,23 +30,22 @@ function LinksHomePage() {
     )
   })
 
-
-
-
-  
   return (
     <>
       <HeaderComponent/>
-
       <section>
         <h2 style={{color: 'blue'}}>Home Page Links</h2>
+        {valueError ? (
+            <h2> Algo salio mal en la peticion... </h2>
+          ) : !valuePostCards.length ? (
+              <h2> Cargando pagina... </h2>
+          ) : (
+            <ul>{valuePostCards}</ul>
+          )
+        }
 
-        <ul>
-          {valuePostCards}
-        </ul>
 
       </section>
-
     </>
   )
 }
