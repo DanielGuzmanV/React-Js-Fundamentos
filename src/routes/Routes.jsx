d0d56616cd2faee7b4e6be0cc9dfc495bf4c417c
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-12 09:06:39 -0400

    se agrego un lazy a las rutas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,47 +1,52 @@
 import { Route, Routes } from "react-router-dom"
-import HomePage from "../pages/HomePage"
-import PageMenu from "../pages/PageMenu"
-import AtributoReactivo from "../components/AtributoReactivo"
-import Renderizado from "../components/Renderizado"
-import Listas from "../components/Listas"
-import PagePropsPadrehijo from "../pages/PagePropsPadrehijo"
-import ClassComponent from "../components/ClassComponent"
-import PagePropsHijoPadre from "../pages/PagePropsHijoPadre"
-import PageStateUp from "../pages/PageStateUp"
-import PageUseEffect from "../pages/PageUseEffect"
-import PageFetchapi from "../pages/PageFetchapi"
-import PageFormularios from "../pages/PageFormularios"
-import LinksHomePage from "../pages/LinksHomePage"
-import LinksBlogPage from "../pages/LinksBlogPage"
-import PostDetails from "../pages/PostDetails"
-import LinksSettingsPage from "../pages/LinksSettingsPage"
-import ErrorPage from "../pages/ErrorPage"
+import { lazy, Suspense } from "react"
+
+import HomePage from "../pages/HomePage";
+
+const AtributoReactivo = lazy(() => import("../components/AtributoReactivo"));
+const Renderizado = lazy(() => import("../components/Renderizado"));
+const Listas = lazy(() => import("../components/Listas"));
+const ClassComponent = lazy(() => import("../components/ClassComponent"));
+const PageMenu = lazy(() => import("../pages/PageMenu"));
+const PagePropsPadrehijo = lazy(() => import("../pages/PagePropsPadrehijo"));
+const PagePropsHijoPadre = lazy(() => import("../pages/PagePropsHijoPadre"));
+const PageStateUp = lazy(() => import("../pages/PageStateUp"));
+const PageUseEffect = lazy(() => import("../pages/PageUseEffect"));
+const PageFetchapi = lazy(() => import("../pages/PageFetchapi"));
+const PageFormularios = lazy(() => import("../pages/PageFormularios"));
+const LinksHomePage = lazy(() => import("../pages/LinksHomePage"));
+const LinksBlogPage = lazy(() => import("../pages/LinksBlogPage"));
+const PostDetails = lazy(() => import("../pages/PostDetails"));
+const LinksSettingsPage = lazy(() => import("../pages/LinksSettingsPage"));
+const ErrorPage = lazy(() => import( "../pages/ErrorPage"));
+
 
 function MyRoutes() {
   return (
-
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/menu" element={<PageMenu/>}/>
-      <Route path="/formBasico" element={<AtributoReactivo/>}/>
-      <Route path="/renderizado" element={<Renderizado/>}/>
-      <Route path="/listas" element={<Listas/>}/>
-      <Route path="/propsPH" element={<PagePropsPadrehijo/>}/>
-      <Route path="/useClass" element={<ClassComponent/>}/>
-      <Route path="/propsHP" element={<PagePropsHijoPadre/>}/>
-      <Route path="/stateUp" element={<PageStateUp/>}/>
-      <Route path="/useEffect" element={<PageUseEffect/>}/>
-      <Route path="/fetch" element={<PageFetchapi/>}/>
-      <Route path="/formulario" element={<PageFormularios/>}/>
-      <Route path="/linksBlog" element={<LinksBlogPage/>}/>
-      <Route path="/linksHome" element={<LinksHomePage/>}/>
-      <Route path="/linksHome/:id" element={<PostDetails/>}/>
-      <Route path="/linksSettings" element={<LinksSettingsPage/>}/>
-
-      <Route path="*" element={<ErrorPage/>}/>
+    <Suspense fallback={<h1>Loading page...</h1>}>
       
-    </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/menu" element={<PageMenu/>}/>
+        <Route path="/formBasico" element={<AtributoReactivo/>}/>
+        <Route path="/renderizado" element={<Renderizado/>}/>
+        <Route path="/listas" element={<Listas/>}/>
+        <Route path="/propsPH" element={<PagePropsPadrehijo/>}/>
+        <Route path="/useClass" element={<ClassComponent/>}/>
+        <Route path="/propsHP" element={<PagePropsHijoPadre/>}/>
+        <Route path="/stateUp" element={<PageStateUp/>}/>
+        <Route path="/useEffect" element={<PageUseEffect/>}/>
+        <Route path="/fetch" element={<PageFetchapi/>}/>
+        <Route path="/formulario" element={<PageFormularios/>}/>
+        <Route path="/linksBlog" element={<LinksBlogPage/>}/>
+        <Route path="/linksHome" element={<LinksHomePage/>}/>
+        <Route path="/linksHome/:id" element={<PostDetails/>}/>
+        <Route path="/linksSettings" element={<LinksSettingsPage/>}/>
 
+        <Route path="*" element={<ErrorPage/>}/>
+        
+      </Routes>
+    </Suspense>
   )
 }
 
