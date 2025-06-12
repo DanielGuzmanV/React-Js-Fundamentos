ae3b7c7057959c0a5cc7768952a45da90611433b
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-11 20:37:34 -0400

    se agrego el registerSettings

--- src/pages/LinksBlogPage.jsx
+++ src/pages/LinksBlogPage.jsx
@@ -2,21 +2,17 @@ import { Link } from "react-router-dom"
 import HeaderComponent from "../components/HeaderComponent"
 import { useContext } from "react"
 import { UserContext } from "../context/user_context"
+import RegisterSettings from "../components/RegisterSettings"
 
 function LinksBlogPage() {
-  const {user, setDataUser, loginUser, logoutUser} = useContext(UserContext)
 
   return (
     <>
       <HeaderComponent/>
       <h2 style={{color: 'blue'}}>Blog page Links</h2>
 
-      <h3> Registarse: </h3>
-      <p>Nombre de usuario: {user && <b>{user.nombre}</b>}</p>
+      <RegisterSettings/>
 
-      <button onClick={() => loginUser()}> Iniciar sesion </button>
-      <button onClick={() => logoutUser()}> Cerrar sesion </button>
-      
       <ul>
         <h3>Lista del blog</h3>
         <li>
