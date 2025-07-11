b9c5843df569f8a716aab71ad4e25bf5a4c09bc2
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-07-10 22:49:48 -0400

    App principal modificado

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import MyRoutes from "./routes/Routes"
-import reactLogo from "./assets/react.svg"
-import { useNavigate } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
-  const mainNavigation = useNavigate(); 
-
-  // Evento Independiente y reutilizable:
-  const greetConsole = () => {
-    console.log('Saludo desde la consola');
-    mainNavigation('/')
-  }
-
   return (
     <div>
-      <h1 onClick={greetConsole} >
-        Inicio del proyecto
-        <img style={{
-          width: '50px',
-          padding: '8px'
-        }} src={reactLogo}/>
-      </h1>
-
+      
+      {/* Scroll automatico */}
       <ScrollToTop/>
       <MyRoutes/>
       <hr/>
